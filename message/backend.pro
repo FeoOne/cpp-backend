64d42dc9f74fbc331362b33453ed7e31e5f1a482
Aleksandr Feoktistov <fe0@ya.ru>
2018-10-01 01:54:43 +0400

    reworked project structure and naming

---
+++ message/backend.pro
@@ -0,0 +1,8 @@
+message handshake_request {
+    message: string[16];
+};
+
+message handshake_response {
+    message: bytes[16];
+    version: u32;
+}
--- tools/messagen/src/main/main.cpp
+++ tools/messagen/src/main/main.cpp
@@ -5,3 +5,36 @@
  * @brief
  */
 
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <streambuf>
+
+#include "main/lexertk.h"
+
+int main(int argc, char **argv)
+{
+    const char *filepath = argv[1];
+
+    std::ifstream file { filepath };
+    std::string content { (std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>() };
+
+    lexertk::generator generator;
+
+    if (!generator.process(content)) {
+        std::cout << "Failed to lex: " << content << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    lexertk::helper::bracket_checker bc;
+    bc.process(generator);
+    if (!bc.result()) {
+        std::cout << "Failed Bracket Checker!" << std::endl;
+        return 1;
+    }
+
+    lexertk::helper::dump(generator);
+
+    return EXIT_SUCCESS;
+}
